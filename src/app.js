b4a4fa2507b93cc8cb48336a0e9c8134f3f317e0
Iryna Knyzh <iryna.knyzh@gmail.com>
2026-02-10 12:59:42 +0200

    task

--- src/app.js
+++ src/app.js
@@ -1,1 +1,59 @@
 // write code here
+/* eslint-disable no-console */
+const fs = require('fs/promises');
+const path = require('path');
+
+async function isDirectoryExists(directoryPath) {
+  try {
+    const stats = await fs.stat(directoryPath);
+
+    return stats.isDirectory();
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      return false;
+    }
+    throw error;
+  }
+}
+
+async function moveFile() {
+  const [, , source, destination] = process.argv;
+
+  if (!source || !destination) {
+    console.error('Please provide both source and destination paths.');
+
+    return;
+  }
+
+  if (source === destination) {
+    return;
+  }
+
+  const lastChar = destination[destination.length - 1];
+  let newDestination = destination;
+
+  if (lastChar === '/') {
+    const stat = await fs.stat(destination);
+
+    if (!stat.isDirectory()) {
+      throw new Error('directory not exists');
+    }
+
+    newDestination = destination + path.basename(source);
+  } else {
+    const directoryExists = await isDirectoryExists(destination);
+
+    if (directoryExists) {
+      newDestination = destination + '/' + path.basename(source);
+    }
+  }
+
+  try {
+    await fs.rename(source, newDestination);
+    console.log(`Moved '${source}' to '${newDestination}'`);
+  } catch (error) {
+    console.error(`Error moving file: ${error.message}`);
+  }
+}
+
+moveFile();
